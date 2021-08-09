9cb65982cb64012627e68ba544bbf147855a7500
Shkha Sharma <37495681+Xavarian@users.noreply.github.com>
2021-08-09 23:27:07 +0530

    Python as Calculator, String Definition

---
+++ 2021-08-09/pro3.py
@@ -0,0 +1,16 @@
+'''
+Q. WHat is String?
+Ans. 
+SS: Jo bhi chiz ko double quote ya single quote me likh skte hai
+
+CM: String is a sequence of characters.
+
+String is immutable
+'''
+
+
+name = "CMPundhir"
+print(name)
+
+name = "CM"
+print(name)
